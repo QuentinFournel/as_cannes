d6f20f2a163562cc50c4d345f02137ea643fbcb9
Quentin FOURNEL <55802451+QuentinFournel@users.noreply.github.com>
2025-08-07 09:44:47 +0200

    Add files via upload

--- ams.py
+++ ams.py
@@ -2773,17 +2773,22 @@ def streamlit_application(all_df):
 
             col1, col2, col3, col4 = st.columns(4)
 
+            age_value = compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Âge'].values[0]
+            taille_value = compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Taille'].values[0]
+            pied_value = compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Pied'].values[0]
+            contrat_value = compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Contrat expiration'].values[0]
+
             with col1:
-                bordered_metric(col1, "Âge", int(compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Âge'].values[0]), 165)
+                bordered_metric(col1, "Âge", '?' if pd.isna(age_value) or age_value == 0 or age_value == '' else int(age_value), 165)
 
             with col2:
-                bordered_metric(col2, "Taille", int(compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Taille'].values[0]), 165)
+                bordered_metric(col2, "Taille", '?' if pd.isna(taille_value) or taille_value == 0 or taille_value == '' else int(taille_value), 165)
 
             with col3:
-                bordered_metric(col3, "Pied fort", compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Pied'].values[0].capitalize(), 165)
+                bordered_metric(col3, "Pied fort", '?' if pd.isna(pied_value) or pied_value == 0 or pied_value == '' else pied_value.capitalize(), 165)
 
             with col4:
-                bordered_metric(col4, "Contrat expiration", compute_weighted_stats_by_minutes(df[df['Joueur + Information'] == joueur])['Contrat expiration'].values[0], 165)
+                bordered_metric(col4, "Contrat expiration", '?' if pd.isna(contrat_value) or contrat_value == 0 or contrat_value == '' else contrat_value, 165)
 
             st.markdown("---")
 
