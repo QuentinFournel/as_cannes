2de9a2690413fc7d82f00ed24c8c5385c06b4a5c
Quentin FOURNEL <55802451+QuentinFournel@users.noreply.github.com>
2026-01-05 15:52:31 +0100

    Add files via upload

--- ams.py
+++ ams.py
@@ -3159,7 +3159,6 @@ def streamlit_application(all_df_dict):
         st.selectbox(
             "Saison",
             ["24-25", "25-26"],
-            index=["24-25", "25-26"].index(st.session_state.get("saison", "25-26")),
             key="saison",
             label_visibility="collapsed"
         )
