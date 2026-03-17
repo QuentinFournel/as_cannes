9fcaad78fe73a64a2bc8db4e3e714ea6b529cc42
Quentin FOURNEL <55802451+QuentinFournel@users.noreply.github.com>
2026-03-17 11:18:24 +0100

    Add files via upload

--- ams.py
+++ ams.py
@@ -2281,7 +2281,7 @@ def plot_team_metrics(df, x_metric, y_metric):
         "St Maur Lusitanos": "https://upload.wikimedia.org/wikipedia/fr/8/89/Logo_US_Lusitanos_Saint-Maur_2018.svg",
         "Nîmes": "https://upload.wikimedia.org/wikipedia/fr/f/f0/N%C3%AEmes_Olympique_logo_2018.svg",
         "FC 93 Bobigny BG": "https://upload.wikimedia.org/wikipedia/fr/d/d7/Logo_Football_Club_93-2024.png",
-        "Rousset-Ste Victoire": "https://fcroussetsvo.fr/img/rousset.png",
+        "Rousset-Ste Victoire": "https://fr.wikipedia.org/wiki/Rousset_(Bouches-du-Rh%C3%B4ne)#/media/Fichier:Blason_de_la_ville_de_Rousset_(13).svg",
         "Limonest": "https://upload.wikimedia.org/wikipedia/fr/7/7c/Logo_FC_Limonest_Dardilly_Saint_Didier_-_2021.svg",
         "Créteil": "https://upload.wikimedia.org/wikipedia/fr/9/99/Logo_US_Cr%C3%A9teil_Lusitanos_2015.svg"
     }
