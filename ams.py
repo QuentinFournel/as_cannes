4cb2a146883506f9315f0ec114b1c6d9dd228310
Quentin FOURNEL <55802451+QuentinFournel@users.noreply.github.com>
2026-06-30 16:21:52 +0200

    Add files via upload

--- ams.py
+++ ams.py
@@ -2381,7 +2381,7 @@ def search_recommended_players(df, poste, thresholds):
 
     df_ranked = rank_columns(df_filtré)
 
-    df_scores = df_ranked[['Joueur + Information', 'Âge', 'Taille', 'Minutes jouées', 'Contrat expiration'   ] + list(thresholds.keys())].copy()
+    df_scores = df_ranked[['Joueur + Information', 'Âge', 'Taille', 'Passeport pays', 'Pied', 'Minutes jouées', 'Contrat expiration'] + list(thresholds.keys())].copy()
 
     for métrique, seuil in thresholds.items():
         df_scores = df_scores[df_scores[métrique] >= seuil]
